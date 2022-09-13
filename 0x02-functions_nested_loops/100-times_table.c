1f66bee44b9f7cd0de290401f175b8dcec17284a
Oladotun Olajimbiti <oladotunhero@gmail.com>
2022-09-13 06:05:02 -0700

    fixed error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,20 @@
-#include "holberton.h"
-
-
-
+#include "main.h"
 /**
-
  * print_times_table - print multiplication table up to n
-
  * @n: integer argument
-
  */
-
-
-
 void print_times_table(int n)
-  
 {
-  
-  int row;
-  
-  int column;
-  
-  int product;
-  
-
-  
-  if (n >= 0 && n <= 15)
-    
-    {
-      
-      for (row = 0; row <= n; row++)
-	
-	{
-	  
-	  for (column = 0; column <= n; column++)
-	    
-	    {
-	      
-	      product = (row * column);
+int row;
+int column;
+int product;
+if (n >= 0 && n <= 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = (row * column);
 if (column == 0)
 _putchar('0' + product);
 else
