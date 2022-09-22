b07705ac1f6495ba3b5e85a3b254e3d93a113010
Oladotun Olajimbiti <oladotunhero@gmail.com>
2022-09-22 11:55:48 -0700

    new code

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,8 +6,9 @@
  */
 char *string_toupper(char *s)
 {
-while (*s) /*if s exists will omit null terminator*/
-if (*s >= 'a' && *s <= 'z')
-*s = *s - 'a' + 'A';
-s++;
+int i;
+for (i = 0; s[i] != '\0'; i++)
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 32;
+return (s);
 }
