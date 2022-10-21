0ef998a2b84235f261e2d3e4b5fe3a9a9caf1a37
Oladotun Olajimbiti <oladotunhero@gmail.com>
2022-10-20 19:27:35 -0700

    fix issue

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-int count = 0;
+size_t nelem;
+nelem = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[0] (nil)\n");
+printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-count++;
+nelem++;
 }
-return (count);
+return (nelem);
 }
