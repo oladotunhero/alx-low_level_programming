797f692c0a43e381cb13aedc3845fe8c2af2712e
Oladotun Olajimbiti <oladotunhero@gmail.com>
2022-12-09 02:33:03 +0000

    updated task 0 file and created lists file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,24 +1,19 @@
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
+ * print_dlistint - prints all data of doubly linked list
+ * @h: pointer to head of list
+ * Return: number of elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-int count;
-count = 0;
-if (h == NULL)
-return (count);
-while (h->prev != NULL)
-h = h->prev;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-count++;
-h = h->next;
-}
-return (count)
+  size_t nodes = 0;
+
+  while (h)
+    {
+      printf("%d\n", h->n);
+      h = h->next;
+      nodes += 1;
+    }
+
+  return (nodes);
 }
